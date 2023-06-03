a9b8fb7d73b6dc3e1a281659107677ca54e6e046
itsprogrammerbro <contactprogrammerbro@gmail.com>
2023-06-03 21:01:43 +0530

    Removed Jquery and fixed burger button click

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,18 +1,13 @@
 import { Component } from '@angular/core';
-import * as $ from "jquery";
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent {
+  status:boolean = true;
   burgerClick(){
-    // Check for click events on the navbar burger icon
-    $(".navbar-burger").on("click",function() {
-      // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
-      $(".navbar-burger").toggleClass("is-active");
-      $(".navbar-menu").toggleClass("is-active");
-    });
+   this.status=!this.status
   }
   
 }
